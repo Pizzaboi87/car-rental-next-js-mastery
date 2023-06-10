f9825cdd353f97ad8eadd20da00fe54b893b8435
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-06-10 20:31:09 +0300

    started to build hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,29 @@
+'use client';
+
+import CustomButton from './CustomButton';
 import Image from 'next/image';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const handleScroll = () => {};
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car -- quickly and easily.
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+        <CustomButton
+          title="Explore cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
