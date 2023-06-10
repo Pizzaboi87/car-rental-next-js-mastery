c4f32febf4f38a1fa9e3019735df86032f3a79d1
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-06-10 20:34:34 +0300

    started to build hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,6 +22,11 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="" />
+        </div>
+      </div>
     </div>
   );
 };
