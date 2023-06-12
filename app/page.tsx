ee42c75646aae8d3ed4dd0b1e7a4d23bc4394c64
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-06-12 16:19:08 +0300

    started to use api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+'use client';
 import { Hero, SearchBar, CustomFilter } from '@/components';
+import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  console.log(allCars);
+
   return (
     <main className="overflow-hidden">
       <Hero />
