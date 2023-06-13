49f6ed473e237726cc4b63464594e3b1ecb3e820
Peter Weiser <112249710+Pizzaboi87@users.noreply.github.com>
2023-06-13 09:28:15 +0300

    error handling

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default async function Home() {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            {/*<CustomFilter title="fuel" />
+            <CustomFilter title="year" />*/}
           </div>
         </div>
         {!isDataEmpty ? (
